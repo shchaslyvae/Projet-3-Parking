b6d8f3b4f223b1bd8c23a24a066140aa9f1155d9
Emilia <shchaslyvaemilia@gmail.com>
2025-11-14 09:27:00 +0100

    Premier commit

---
+++ voiture.py
@@ -0,0 +1,47 @@
+class Voiture:
+    def __init__(self, immatriculation:str, marque:str, proprietere:str  ):
+        self.immatriculation = immatriculation
+        self.marque = marque
+        self.proprietere = proprietere
+        self.abone = False
+        self.place = ""
+    
+    def immatriculation(self):
+        return self.immatriculation
+    
+    def marque(self):
+        return self.marque
+    
+    def proprietere(self):
+        return self.proprietere
+    
+    def abone(self):
+        return self.abone
+    
+    def num_place(self):
+        return self.place 
+
+
+class Parking:
+    def __int__ (self, voiture:Voiture):
+        self.niveau = "0"
+        self.voitures = []
+        self.voiture = voiture
+
+
+    def get_immatriculation(self):
+        return self.voiture.immatriculation
+    
+    def get_marque(self):
+        return self.voiture.marque
+    
+    def get_proprietere(self):
+        return self.voiture.proprietere
+    
+    def get_abone(self):
+        return self.voiture.abone
+    
+    def get_num_place(self):
+        return self.voiture.place 
+
+    
