08c0a5273edd208e8d5992300de800b0b17eaaf0
Emilia <shchaslyvaemilia@gmail.com>
2025-11-21 09:55:26 +0100

    commit3

--- voiture.py
+++ voiture.py
@@ -1,20 +1,20 @@
-from random import*
+import random
 class Voiture:
-    def __init__(self, immatriculation:str, marque:str, proprietere:str ):
+    def __init__(self, immatriculation:str, marque:str, proprietaire:str ):
         self.immatriculation = immatriculation
         self.marque = marque
-        self.proprietere = proprietere
+        self.proprietaire = proprietaire
         self.abone = False
         self.place = ""
     
-    """
+    
     def immatriculation(self):
         return self.immatriculation
     
     def marque(self):
         return self.marque
     
-    def proprietere(self):
+    def proprietaire(self):
         return self.proprietere
     
     def abone(self):
@@ -23,43 +23,110 @@ class Voiture:
     def num_place(self):
         return self.place 
 
-    """
+    
 
 
 class Parking:
-    def __init__ (self, voiture:Voiture):
+    def __init__ (self ):
         self.niveau = "0"
         self.voitures = []
-        self.voiture = voiture
+        self.voiture = ""
+        self.places_occupees = []
 
 
-    def get_immatriculation(self):
-        return self.voiture.immatriculation
+    def get_immatriculation(self, voiture): #marche
+        """méthode pour afficher mmatriculation"""
+        return voiture.immatriculation
     
-    def get_marque(self):
-        return self.voiture.marque
+    def get_marque(self, voiture): #marche
+        return voiture.marque
     
-    def get_proprietere(self):
-        return self.voiture.proprietaire
+    def get_proprietere(self, voiture): #marche
+        return voiture.proprietaire
     
-    def est_abonee(self):
-        return self.voiture.abone
+    def est_abonee(self, voiture): #marhce
+        return voiture.abone 
     
-    def get_num_place(self):
-        return self.voiture.place 
+    def get_num_place(self, voiture): #marche
+        return voiture.place 
     
-    def abonner(self, voiture: Voiture):
+    def abonner(self, voiture: Voiture): #marche
         """méthode qui permett d'abonner ou d'annuler un abonnement """
-        if voiture.est_abonee():
+        if self.est_abonee(voiture):
             voiture.abone = False
             print("abonnement annulé")
         else: 
-            voiture.abonne = True
+            voiture.abone = True
             print("abonné")
 
-    def placer(self):
-        self.place = random.randint(0,80)
-        self.etages 
+    def placer(self, voiture): #marche 
+        val = random.randint(0, 80)
+        self.place = val
+        self.etage = random.randint(0, 4)
+        self.place_etage = self.place + (self.etage*100)
+        self.places_occupees.append(self.place_etage)
+        voiture.place = self.place_etage
+
+    
+    def place_occupee_voiture(self):
+        for i in self.place_occupees:
+            if i == self.place_etage:
+                return self.get_immatriculation()
+            
+    def liste_place_abonnee(self):
+        return self.place_occupees
+
+    """
+    def place_libre(self):
+        place_libre = []
+        for i in self.places_occupees:
+            if i == self.place_etage:
+                return 
+    """
+            
+    def places_libres_sans_abonnement(self):
+        self.places = []
+        self.place_liste = 1
+        self.niveau_liste = 0
+        for i in range(5):
+            for j in range(80):
+                self.places.append(self.niveau_liste+self.niveau_liste)
+                self.place_liste += 1
+            self.niveau_liste += 100
+            self.place_liste = 1
+        for i in self.place_occupees:
+            self.places.remove(i)
+        return self.places
+    
+    def representer_niveau(self):
+        return self.niveau_liste
+        
+        
+
+voiture1 = Voiture("AA-001-AA", "BMW", "M. Informatique")
+voiture2 = Voiture("AB-565-AB", "audi", "Piere")
+parking = Parking()
+parking.placer(voiture1)
+parking.placer(voiture2)
+#test 
+"""
+print(parking.get_num_place(voiture1))
+print(parking.get_num_place(voiture2))
+print(parking.get_marque(voiture1))
+print(parking.get_marque(voiture2))
+print(parking.get_immatriculation(voiture1))
+print(parking.get_immatriculation(voiture2))
+print(parking.get_proprietere(voiture1))
+"""
+print(parking.est_abonee(voiture1))
+parking.abonner(voiture1)
+parking.abonner(voiture1)
+
+        
+    
+        
+    
+                
 
     
             
